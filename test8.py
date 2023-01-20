44de5b285853ed70b36254b28933a908c64465e3
V1it1a1l1i1y2 <123171890+V1it1a1l1i1y2@users.noreply.github.com>
2023-01-20 15:44:08 +0300

    Add files via upload

---
+++ test8.py
@@ -0,0 +1,37 @@
+import random
+def печатать(pabyj):
+	print(pabyj)
+def вещественное_число(ypypyp):
+	float(ypypyp)
+	return
+import random
+def печатать(pabyj):
+	print(pabyj)
+def вещественное_число(ypypyp):
+	float(ypypyp)
+	return
+def целое_число (etyetyeteyftygftgh):
+	int(etyetyeteyftygftgh)
+	return
+def рандом(rtyuiiop,qwertt):
+	random.randint(rtyuiiop,qwertt)
+def в_список (ppppeeeetttt,pepepepepepeepepeddddddd):
+	ppppeeeetttt.append(pepepepepepeepepeddddddd)
+def сколько_в_списке (pttptptororororo):
+	len(pttptptororororo)
+	return
+def удалить_из_списка (pppppppppppppppppppppppppppppppppp,iririririri):
+	pppppppppppppppppppppppppppppppppp.remove(iririririri)
+def максимум (ppppppppppppppppppppppppppppppppppppppppppppppppp):
+	max(ppppppppppppppppppppppppppppppppppppppppppppppppp)
+	return
+def минимум (ppppppppppppppppppppppppppppppppppppppppppppppppppppppp):
+	min(ppppppppppppppppppppppppppppppppppppppppppppppppppppppp)
+	return
+def определить_одинаковоеь (jghtlbkbnm,jlbyfrjdjt):
+	jghtlbkbnm.count(jlbyfrjdjt)
+	return
+def обратить_список(jmhfnbnmcgbcjr):
+	jmhfnbnmcgbcjr.reverse()
+def дзен_Пайтона ():
+	import this
